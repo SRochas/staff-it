284c1181e3c87344b9b06822343cfa8995753037
Sam Rockwell <samrockwell@Sams-MBP.lan>
2022-12-21 22:19:46 -0500

    Start using a card

--- stories/ReportCard.tsx
+++ stories/ReportCard.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import LinearProgress from '@mui/material/LinearProgress';
-import Button from '@mui/material/Button'
+import React from "react";
+import LinearProgress from "@mui/material/LinearProgress";
+import Card from "@mui/material/Card";
 
-interface ReportCardProps {
-}
+interface ReportCardProps {}
 
 /**
  * Primary UI component for user interaction
  */
-export const ReportCard = ({
-}: ReportCardProps) => {
-return <Button >Hey there</Button>
+export const ReportCard = ({}: ReportCardProps) => {
+  return (
+    <Card variant="outlined" sx={{ width: 200 }}>
+      <h1>The new project</h1>
+      <LinearProgress variant="determinate" value={25} color="error" />;
+    </Card>
+  );
 };
