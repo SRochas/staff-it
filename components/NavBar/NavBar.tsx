e503bfc2c45bf3e1325216caef29272e142cdb7a
Sam Rockwell <samrockwell@Sams-MBP.lan>
2023-01-03 22:21:09 -0500

    Make NavBar more generic and customizable

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,11 +1,35 @@
+import React, { ReactNode, useState } from "react";
+import type { MouseEvent } from "react";
 import AppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
-const NavBar = () => {
+export type NavBarProps = {
+  children?: ReactNode | ReactNode[];
+  onMenuClose: (event: MouseEvent<HTMLElement>) => void;
+  onMenuOpen: (event: MouseEvent<HTMLElement>) => void;
+  title: string;
+  anchorElNav: HTMLElement;
+};
+
+const NavBar = ({
+  children,
+  title,
+  onMenuClose,
+  onMenuOpen,
+  anchorElNav,
+}: NavBarProps) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn((prev: boolean) => !prev);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -15,13 +39,36 @@ const NavBar = () => {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
+          onClick={onMenuOpen}
         >
           <MenuIcon />
         </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={onMenuClose}
+          sx={{
+            display: { xs: "block", md: "none" },
+          }}
+        >
+          {children}
+        </Menu>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          News
+          {title}
         </Typography>
-        <Button color="inherit">Login</Button>
+        <Button color="inherit" onClick={handleLogin}>
+          {isLoggedIn ? "Log Out" : "Login"}
+        </Button>
       </Toolbar>
     </AppBar>
   );
--- stories/NavBar.stories.tsx
+++ stories/NavBar.stories.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import type { MouseEvent } from "react";
 import type { Meta, Story } from "@storybook/react";
 import createTheme from "@mui/material/styles/createTheme";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import CssBaseline from "@mui/material/CssBaseline/CssBaseline";
 import { useDarkMode } from "storybook-dark-mode";
 import NavBar from "../components/NavBar/NavBar";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+
+const pages = ["Products", "Pricing", "Blog"];
 
 type TemplateProps = {};
 
@@ -29,7 +34,34 @@ export default {
   decorators: [StoryWithTheme],
 } as Meta;
 
-const Template: Story<TemplateProps> = ({}) => <NavBar />;
+const Template: Story<TemplateProps> = ({}) => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [currentTitle, setCurrentTitle] = useState(pages[0]);
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleClickMenuItem =
+    (title?: string) => (event: MouseEvent<HTMLElement>) => {
+      if (title) {
+        setCurrentTitle(title);
+      }
+      setAnchorElNav(null);
+    };
+  return (
+    <NavBar
+      onMenuClose={handleClickMenuItem()}
+      title={currentTitle}
+      anchorElNav={anchorElNav}
+      onMenuOpen={handleOpenNavMenu}
+    >
+      {pages.map((page) => (
+        <MenuItem key={page} onClick={handleClickMenuItem(page)}>
+          <Typography textAlign="center">{page}</Typography>
+        </MenuItem>
+      ))}
+    </NavBar>
+  );
+};
 
 export const BoardTemplate: Story<TemplateProps> = Template.bind({});
 
